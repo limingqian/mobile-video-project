1dd7f5c7902518804b77af8628a50be2efc625a4
mingqian.li <abc350082039@qq.com>
2021-03-17 11:10:47 +0800

    feat:解决一节课多个视频的问题

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="detail">
     <van-nav-bar title="VR全景制作" left-arrow @click-left="onClickLeft" />
-    <video-player
-      class="video-player vjs-custom-skin"
-      ref="videoPlayer"
-      :playsinline="true"
-      :options="playerOptions"
-      @play="onPlayerPlay($event)"
-      @pause="onPlayerPause($event)"
-      @ended="onPlayerEnded($event)"
-    >
-    </video-player>
+    <van-tabs>
+      <van-tab v-for="i in playerOptions" :title="i.name" :key="i.id">
+        <video-player
+          class="video-player vjs-custom-skin"
+          ref="videoPlayer"
+          :playsinline="true"
+          :options="i"
+          @play="onPlayerPlay($event)"
+          @pause="onPlayerPause($event)"
+          @ended="onPlayerEnded($event)"
+        >
+        </video-player>
+      </van-tab>
+    </van-tabs>
     <van-tabs v-model="active">
       <!-- 介绍 -->
       <van-tab title="介绍">
@@ -29,13 +33,7 @@
             />
           </div>
         </div>
-        <!-- white-space:pre-line; 处理换行 -->
-        <!-- <div
-          style="text-align:left;margin:1rem 0.5rem 1rem 1rem;white-space:pre-line;width:84vw;"
-        > -->
         <div ref="contentInner"></div>
-        <!-- {{ introduction.content }}
-        </div> -->
         <div class="line"></div>
         <div class="teacher">
           <div class="buffer" style="text-align:left;font-size:1.2rem;">
@@ -129,11 +127,10 @@ export default {
         content: ""
       },
       teacher: {
-        name: "李老师",
+        name: "",
         img: "",
-        position: "首席虚拟现实讲师",
-        introduction:
-          "有时候，通过一个名称来标识一个路由显得更方便一些，特别是在链接一个路由，或者是执行一些跳转的时候。你可以在创建 Router 实例的时候，在 routes 配置中给某个路由设置名称"
+        position: "",
+        introduction: ""
       },
       items: [
         {
@@ -150,31 +147,7 @@ export default {
         }
       ],
       learnItems: [],
-      playerOptions: {
-        playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
-        autoplay: false, // 如果为true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: false, // 是否视频一结束就重新开始。
-        controls: true,
-        preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: "zh-CN",
-        aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [
-          {
-            type: "video/mp4", // 类型
-            src: "" // url地址
-          }
-        ],
-        poster: "", // 封面地址
-        notSupportedMessage: "此视频暂无法播放，请稍后再试", // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
-        controlBar: {
-          timeDivider: true, // 当前时间和持续时间的分隔符
-          durationDisplay: true, // 显示持续时间
-          remainingTimeDisplay: false, // 是否显示剩余时间功能
-          fullscreenToggle: true // 是否显示全屏按钮
-        }
-      }
+      playerOptions: []
     };
   },
   computed: {
@@ -198,6 +171,35 @@ export default {
     });
   },
   methods: {
+    creatObj(url, name, id) {
+      return {
+        id,
+        name,
+        playbackRates: [0.5, 1.0, 1.5, 2.0], // 可选的播放速度
+        autoplay: false, // 如果为true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 是否视频一结束就重新开始。
+        controls: true,
+        preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: "zh-CN",
+        aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [
+          {
+            type: "video/mp4", // 类型
+            src: url // url地址
+          }
+        ],
+        poster: "", // 封面地址
+        notSupportedMessage: "此视频暂无法播放，请稍后再试", // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true, // 当前时间和持续时间的分隔符
+          durationDisplay: true, // 显示持续时间
+          remainingTimeDisplay: false, // 是否显示剩余时间功能
+          fullscreenToggle: true // 是否显示全屏按钮
+        }
+      };
+    },
     async getVideo() {
       this.courseId = this.$route.params.courseId;
       let resultTemp = await courseDetail({
@@ -217,18 +219,23 @@ export default {
         this.teacher.position = entity.teacher.education;
         this.teacher.introduction = entity.teacher.career;
         // TODO 多个视频
-        console.log("++++++++++");
-        console.log(entity.kpointList[0].videoUrl);
-        console.log("++++++++++");
-        this.playerOptions.sources[0].src =
-          baseUrl + entity.kpointList[0].videoUrl;
+        this.playerOptions.push(
+          this.creatObj(
+            baseUrl + entity.kpointList[0].videoUrl,
+            entity.kpointList[0].name,
+            1
+          )
+        );
+        this.playerOptions.push(
+          this.creatObj(
+            baseUrl + entity.kpointList[1].videoUrl,
+            entity.kpointList[1].name,
+            2
+          )
+        );
       }
-
-      // this.playerOptions.sources[0].src =
-      //   "https://www.runoob.com/try/demo_source/movie.mp4";
     },
     onClickLeft() {
-      // this.$router.push('/List');
       this.$router.go(-1);
     },
     // listen event
